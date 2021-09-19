513433edd5fc55e615afd1a35354ff89892b31cd
engamibiz <77250446+engamibiz@users.noreply.github.com>
2021-09-19 23:06:13 +0200

    php starting

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+//echo 'hello world ';
+$name='ahmed';
+$integer= -1;
+$float=1.5;
+$boolean=true;
+// echo $boolean;
+
+if($integer > 0) {
+    echo 'Positive';
+}elseif($integer == 0){
+    echo 'Zero';
+}else{
+    echo 'Negative';
+}
+
+
+?>
